932651ef5d41d91485d87583fd5283fc95ebda70
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-08-08 14:55:23 +0300

    v1.0.1+. Почти полная переработка. Добавлено получение и сохранение QuikDeals, QuikOrders, QuikSecurities, QuikAllTrades, KafkaOffsets.

---
+++ src/main/resources/sql/Quik.AllTrades.sql
@@ -0,0 +1,28 @@
+-- Table: Quik.AllTrades
+-- DROP TABLE "Quik"."AllTrades";
+
+CREATE TABLE IF NOT EXISTS "Quik"."AllTrades"
+(
+    "ExchangeCode"      character varying(50) COLLATE pg_catalog."default"  NOT NULL,
+    "TradeNum"          character varying(50) COLLATE pg_catalog."default"  NOT NULL,
+    "Direction"         integer                                                 NULL,
+    "TradeDateTime"     timestamp without time zone                         NOT NULL,
+    "ClassCode"         character varying(50) COLLATE pg_catalog."default"      NULL,
+    "SecCode"           character varying(50) COLLATE pg_catalog."default"      NULL,
+    "Price"             numeric(24,8)                                           NULL,
+    "Quantity"          numeric(24,8)                                           NULL,
+    "Value"             numeric(24,8)                                           NULL,
+    "AccruedInterest"   numeric(24,8)                                           NULL,
+    "Yield"             numeric(24,8)                                           NULL,
+    "SettleCode"        character varying(50) COLLATE pg_catalog."default"  NOT NULL,
+    "RepoRate"          numeric(24,8)                                           NULL,
+    "RepoValue"         numeric(24,8)                                           NULL,
+    "Repo2Value"        numeric(24,8)                                           NULL,
+    "RepoTerm"          integer                                                 NULL,
+    "Period"            smallint                                                NULL,
+    "OpenInterest"      integer                                                 NULL,
+
+    CONSTRAINT "AllTrades_pkey" PRIMARY KEY ("ExchangeCode", "TradeNum")
+) TABLESPACE pg_default;
+
+ALTER TABLE "Quik"."AllTrades" OWNER TO quik;
